e81162edaf932c03e5c33668c93581afd5987201
gspanarin <40568989+gspanarin@users.noreply.github.com>
2018-07-24 16:20:19 +0300

    Add files via upload
    2018.07.24
    + Добавлен класс ЗАПИСЬ САБ ИРБИС64 - irbis64_rec. Класс содержит данные о записи (MFN, статус, версию, список полей)
    + Добавлена функция чтения записи в базе по номеру MFN (до конца не доделано - должна возвращать не текст, а объект irbis64_rec)
    + Добавлена функция сохранения записи в базу

--- example.py
+++ example.py
@@ -1,7 +1,57 @@
-from irbis64_class import irbis64
+from irbis64_class import irbis64, irbis64_rec
 
+def search_record(irbis):
+    retval = irbis.search('ibis','HD=$',10,1,'@brief')
+    if int(retval["status"]) == 0:
+        print('Всего найдено: ' + retval['count'])
+        nom = 1
+        for item in retval["result"]:
+            print('{0}. {1} (MFN={2})'.format(nom,item["rec"],item["mfn"]))
+            nom += 1
+    else:
+        print('error')
+
+def read_record(irbis):
+    retval = irbis.read_record('ibis',1)
+    print(retval)
+         
+
+
+def creat_and_save_record(irbis):
+    rec = irbis64_rec()
+    rec.add_field(920,'PAZK')
+    rec.add_field(900,'^Ta^B05')
+    rec.add_field(102,'UA')
+    rec.add_field(101,'ukr')
+    rec.add_field(999,'00000')
+    rec.add_field(700,'^AЛеся^GУкраїнка')
+    rec.add_field(200,'^AЛісова Пісня')
+    rec.add_field(210,'^D2018^CХарків')
+
+    result = irbis.save_record('mcc', rec)
+    
+    if result > 0:
+        print('OK')
+    else:
+        print('ERROR')
+    
 irbis = irbis64()
+irbis.host = '127.0.0.1'
+irbis.port = 6666
+#Установка отладочного режима:
+#На экран будут выводиться все пакеты
+#irbis.debug = True
+
+#Регистрация на сервере САБ ИРБИС64
 irbis.reg()
-retval = irbis.search('ibis','HD=$',10,1,'@brief')
-print(retval)
+
+#Поиск записи по запросу на языке Ирбис
+#search_record(irbis)
+#Чтение записи в базе по ее номеру (MFN)
+#read_record(irbis)
+#Создание новой записи и сохранение ее в базу данных
+#creat_and_save_record(irbis)
+
+#разрегистрация на сервере САБ ИРБИС64
 irbis.unreg()
+
--- irbis64_class.py
+++ irbis64_class.py
@@ -7,13 +7,13 @@ class irbis64():
 """
     proc_id = random.randint(11111111,99999999)
     command_num = 1
-
+    debug = False
     def __init__(self,
-                 host='127.0.0.1',
-                 port=6666,
-                 arm='C',
-                 login='1',
-                 password='1'):
+                 host = '127.0.0.1',
+                 port = 5555,
+                 arm = 'C',
+                 login = '1',
+                 password = '1'):
         self.host = host
         self.port = port
         self.arm = arm
@@ -78,12 +78,18 @@ class irbis64():
     #  Функция отправки команд на сервер
     ##################################
     def send(self,packet):
+        if self.debug:
+            print('==========================')
+            print(packet)
+            print('==========================')
+
         sock = socket.socket()
         sock.connect((self.host, self.port))
         sock.send(packet.encode())
         tmp = b''
         data = b''
         ################################
+  
         tmp = sock.recv(1024)
         while sock:
             if not tmp:
@@ -92,9 +98,14 @@ class irbis64():
             else:
                 data += tmp
                 tmp = sock.recv(1024)
+
         #########################     
         sock.close()
         self.command_num += 1
+        if self.debug:
+            print(data)
+            print('==========================')
+        
         return data
 
 
@@ -146,4 +157,122 @@ class irbis64():
                 
         return {'status' : status, 'count' : count, 'result' : result}
 
+    def read_record(self,
+                    db_name,
+                    mfn,
+                    lock = 0):
+        """
+        Функция чтения записи по заданной базе и МФН
+"""
+        paramlist = ('C',
+                     self.arm,
+                     'C',
+                     str(self.proc_id),
+                     str(self.command_num),
+                     '',
+                     '',
+                     '',
+                     '',
+                     '',
+                     db_name,
+                     str(mfn),
+                     str(lock))
+        packet = '\n'.join(paramlist)
+        packet = str(len(packet.encode('utf8'))) + '\n' + packet  
+        answer = self.send(packet)
+        answer = answer.decode('utf8')
+        answer = answer.split("\r\n")
 
+        #Это статус выполнения запроса
+        status = int(answer[10])
+        if status == 0:
+            tmp = answer[11]
+            mfn = tmp[:tmp.find('#')]
+            #Это статус текущей записи
+            stat = tmp[tmp.find('#') + 1:]
+
+            tmp = answer[12]
+            ver = tmp[tmp.find('#') + 1:]
+            fields = list()
+            i = 13
+            while i < len(answer):
+                if len(answer[i])>0:
+                    tmp = answer[i]
+                    fieldnum = tmp[:tmp.find('#')]
+                    fieldval = tmp[tmp.find('#') + 1:]
+
+                    fields.append ({'fieldnum' : fieldnum, 'fieldval' : fieldval})
+                i += 1
+                    
+            return {'mfn' : mfn, 'status' : stat, 'ver': ver, 'fields' : fields}      
+        else:
+            return {'error' :'status = ' + status}
+
+    def save_record(self, db_name, rec, Lock = 0, IfUpdate = 1):
+        """
+    Функция сохранения записи в базу САБ ИРБИС64
+    Db_name – имя БД
+    Lock – блокировать ли запись. 1 – блокировать, 0 – не блокировать.
+    IfUpdate – актуализировать ли запись. 1 – актуализировать, 0 – не актуализировать
+"""
+        str_rec = '{0}#{1}{3}0#{2}'.format(rec.mfn, rec.status, rec.version, chr(31)+chr(30))
+        for f in rec.fields:
+            str_rec += '{0}{1}#{2}'.format(chr(31)+chr(30), f['fieldnum'], f['fieldval'])
+        str_rec += chr(31)+chr(30)
+
+        paramlist = ('D',
+                     self.arm,
+                     'D',
+                     str(self.proc_id),
+                     str(self.command_num),
+                     self.password,
+                     self.login,
+                     '',
+                     '',
+                     '',
+                     db_name,
+                     str(Lock),
+                     str(IfUpdate),
+                     str_rec)
+        packet = '\n'.join(paramlist)
+        packet = str(len(packet.encode('utf-8'))) + '\n' + packet  
+        answer = self.send(packet)
+        answer = answer.decode('utf8')
+        answer = answer.split("\r\n")
+ 
+        try:
+            if int(answer[10]) > 0:
+                return int(answer[10])
+            else:
+                return -1 #Ошибка
+        except:
+            return -1 #ошибка
+
+
+class irbis64_rec():
+    """
+    Класс ЗАПИСЬ САБ ИРБИС64
+"""
+    
+    def __init__(self,
+                 mfn = 0,
+                 status = 0,
+                 version = 0,
+                 fields = list()):
+        self.mfn = mfn
+        self.status = status
+        self.version = version
+        self.fields = fields
+
+    def add_field(self,fieldnum,fieldval):
+        self.fields.append({'fieldnum' : fieldnum, 'fieldval' : fieldval})
+
+    def rec_info(self):
+        s = 'mfn = ' + str(self.mfn) + '\n'
+        s += 'status = ' + str(self.status) + '\n'
+        s += 'version = ' + str(self.version) + '\n'
+        s += 'fields : \n'
+        for f in self.fields:
+            s += '\t#{0}: {1}'.format(f['fieldnum'],f['fieldval']) + '\n'
+        return s
+        
